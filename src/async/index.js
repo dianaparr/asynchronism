15ba37d7895b92d0ebe29caeb380350eaca60e4c
dianaparr <dianaparra.9611@gmail.com>
2022-01-14 13:37:40 -0500

    add handling errors wirh async-await

--- src/async/index.js
+++ src/async/index.js
@@ -20,3 +20,18 @@ console.log('After');
 // Before
 // After
 // Do Something Async
+
+
+// 2. Captura de errores
+const anotherFunction = async () => {
+    try {
+        const something = await doSomethingAsync();
+        console.log(something);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+console.log('Before 1');
+anotherFunction();
+console.log('After 1');
