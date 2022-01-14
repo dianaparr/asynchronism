5e37cad9ea2c9aedbf3af80642d0a7fbaa0411a5
dianaparr <dianaparra.9611@gmail.com>
2022-01-14 11:57:49 -0500

    add challenge qith calbacks hell

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,6 +1,10 @@
 // AJAX - Server Response. src: https://www.w3schools.com/xml/ajax_xmlhttprequest_response.asp
 
+// 1. Instanciar o requerir en este doc xmlhttprequest el cual es un objeto de JS que uso para
+// hacer peticiones.
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+const API = 'https://rickandmortyapi.com/api/character/'
 
 function fetchData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
@@ -17,3 +21,18 @@ function fetchData(urlApi, callback) {
     }
     xhttp.send();
 }
+
+// 2. Hacer las peticiones que se requieren
+
+fetchData(API, function(err1, data1) {
+    if (err1) return console.error(err1);
+    fetchData(API + data1.results[0].id, function(err2, data2) {
+        if (err2) return console.error(err2);
+        fetchData(data2.origin.url, function(err3, data3) {
+            if (err3) return console.error(err3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    })
+})
