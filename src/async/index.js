8829938e6eb074b19c6d7ddf00e6b3056dc04d54
dianaparr <dianaparra.9611@gmail.com>
2022-01-14 13:06:00 -0500

    add first example with async-await

---
+++ src/async/index.js
@@ -0,0 +1,22 @@
+const doSomethingAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Do Something Async'), 3000)
+            : reject(new Error('Test Error'))
+    });
+}
+
+const doSomething = async () => {
+    const something = await doSomethingAsync();
+    console.log(something);
+}
+
+console.log('Before');
+doSomething();
+console.log('After');
+
+
+// Output: Se hace la función asincrona y esperamos a que resuelva la promesa
+// Before
+// After
+// Do Something Async
